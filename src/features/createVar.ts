757d03067a1ef6c22b01a3d650f33f580736cfe3
Martin Petrov <martinmpetrov@hotmail.com>
2023-12-02 00:49:08 +0200

    Make All Variables Global

--- src/features/createVar.ts
+++ src/features/createVar.ts
@@ -1,12 +1,25 @@
 // Transform
+import toCss from "../transform/toCss";
 import toVariable from "../transform/toVariable";
 // Utility
+import injector from "../foundation/injector";
 import className from "../foundation/className";
 
-const createVar = () => {
+const inject = injector();
+
+const createVar = (value: string | number = "") => {
   const id = className();
 
   const varName = toVariable(id);
+
+  const result = toCss(":root", {
+    [varName]: value,
+  } as any);
+
+  inject({
+    id: varName,
+    css: result,
+  });
 
   return varName;
 };
